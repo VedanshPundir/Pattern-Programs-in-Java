25dbccf2e6c33a7c41c31f7fbeca52918522bcdf
VedanshPundir <vedanshpundir43@gmail.com>
2024-09-28 19:46:38 +0530

    Patter 1 added

---
+++ Pat1invertedTriangle.java
@@ -0,0 +1,18 @@
+public class Pat1invertedTriangle
+ {
+    public static void main(String args[])
+    {
+        int m=4;
+        for(int i=m;i>=1;i--)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                
+                    System.out.print("*");
+                
+                
+            }
+            System.out.println();
+        }
+    }
+}
