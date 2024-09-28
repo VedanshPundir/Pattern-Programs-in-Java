2577737461318c9e68e28718f0e7afaf24f02120
VedanshPundir <vedanshpundir43@gmail.com>
2024-09-28 21:30:10 +0530

    Rhombus pattern modified

--- Pat5rhombus.java
+++ Pat5rhombus.java
@@ -2,29 +2,20 @@ public class Pat5rhombus
  {
     public static void main(String args[])
     {
-    int n=5;
-    for(int i=1;i<=n;i++)
-    {
-        int space=n-i;
-        for(int j=1;j<=space;j++)
+        int n=5;
+        for(int i=1;i<=n;i++)
         {
+            int space=(n-i);
+           for(int j=1;j<=space;j++)
+           {
             System.out.print(" ");
+           }
+           for(int j=1;j<=n;j++)
+           {
+            System.out.print("*");
+           }
+           System.out.println();
         }
-        for(int j=i;j>=1;j--)
-        {
-            System.out.print(j);
-        }
-        
-    
-        for(int j=2;j<=i;j++)
-        {
-            System.out.print(j);
-        }
-        System.out.println();
-    
-        
     }
-    
-}
     
 }
