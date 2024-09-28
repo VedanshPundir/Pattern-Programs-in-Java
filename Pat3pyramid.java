1b0b80b7092d576460ab532f49f6dc7a6004a7e2
VedanshPundir <vedanshpundir43@gmail.com>
2024-09-28 19:56:32 +0530

    Pattern 3 added

---
+++ Pat3pyramid.java
@@ -0,0 +1,22 @@
+public class Pat3pyramid 
+{
+    public static void main(String args[])
+    {
+        int n=5;
+        for(int i=1;i<=n;i++)
+        {
+            int space=(n-i);
+            for(int j=1;j<=space;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(i);
+                System.err.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
